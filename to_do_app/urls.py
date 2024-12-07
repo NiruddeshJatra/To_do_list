5816bf4512305051780e55a306615e4e7bc1ebd4
Nasiful Alam <nasifulalam1212@gmail.com>
2024-12-08 00:02:24 +0600

    updated

--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -4,10 +4,16 @@ from . import views
 app_name = 'todolist'
 
 urlpatterns = [
-  path('', views.show_tasks, name='show_tasks'),
-  path('completed_tasks/', views.completed_tasks, name='completed_tasks'),
-  path('completed_tasks/<str:taskTitle>/', views.completed_tasks, name='mark_completed'),
-  path('add/', views.add_or_edit_task, name='add_task'),
-  path('delete/<str:taskTitle>/', views.delete_task, name='delete_task'),
-  path('edit/<str:taskTitle>/', views.add_or_edit_task, name='edit_task'),
+  # path('', views.show_tasks, name='show_tasks'),
+  path('', views.showTasks.as_view(), name='show_tasks'),
+  # path('completed_tasks/', views.completed_tasks, name='completed_tasks'),
+  path('completed_tasks/', views.completedTasks.as_view(), name='completed_tasks'),
+  path('complete/<int:pk>/', views.MarkTaskCompleted.as_view(), name='mark_task_completed'),
+  # path('completed_tasks/<str:taskTitle>/', views.completed_tasks, name='mark_completed'),
+  # path('add/', views.add_or_edit_task, name='add_task'),
+  path('add/', views.addTask.as_view(), name='add_task'),
+  # path('delete/<str:taskTitle>/', views.delete_task, name='delete_task'),
+  path('delete/<int:pk>/', views.deleteTask.as_view(), name='delete_task'),
+  # path('edit/<str:taskTitle>/', views.add_or_edit_task, name='edit_task'),
+  path('edit/<int:pk>/', views.updateTask.as_view(), name='edit_task'),
 ]
--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,33 +1,86 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from .models import TaskModel
 from .forms import TaskForm
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views import View
 
 # Create your views here.
-def show_tasks(request):
-  tasks = TaskModel.objects.filter(is_completed=False).all()
-  return render(request, "show_tasks.html", {"tasks": tasks})
+# def show_tasks(request):
+#   tasks = TaskModel.objects.filter(is_completed=False).all()
+#   return render(request, "show_tasks.html", {"tasks": tasks})
 
-def add_or_edit_task(request, taskTitle=None):
-  task = get_object_or_404(TaskModel, taskTitle=taskTitle) if taskTitle else None
-  if request.method == "POST":
-    form = TaskForm(request.POST, instance=task)
-    if form.is_valid():
-      form.save()
-      return redirect("todolist:show_tasks")
-  else:
-    form = TaskForm(instance=task)
+class showTasks(ListView):
+  model = TaskModel
+  template_name = 'show_tasks.html'
+  queryset = TaskModel.objects.filter(is_completed=False).all()
+  context_object_name = 'tasks'
+
+# def add_or_edit_task(request, taskTitle=None):
+#   task = get_object_or_404(TaskModel, taskTitle=taskTitle) if taskTitle else None
+#   if request.method == "POST":
+#     form = TaskForm(request.POST, instance=task)
+#     if form.is_valid():
+#       form.save()
+#       return redirect("todolist:show_tasks")
+#   else:
+#     form = TaskForm(instance=task)
       
-  return render(request, "add_or_edit_task.html", {"form": form, "task": task})
+#   return render(request, "add_or_edit_task.html", {"form": form, "task": task})
 
-def completed_tasks(request, taskTitle=None):
-  if taskTitle:
-    task = get_object_or_404(TaskModel, taskTitle=taskTitle)
+class addTask(CreateView):
+  model = TaskModel
+  fields = '__all__'
+  template_name = "task_form.html"
+  success_url = reverse_lazy("todolist:show_tasks")
+  
+  
+class updateTask(UpdateView):
+  model = TaskModel
+  fields = '__all__'
+  template_name = "task_form.html"
+  success_url = reverse_lazy("todolist:show_tasks")
+  
+  def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      context['task'] = self.object  # 'self.object' is the task being edited
+      return context
+
+# def completed_tasks(request, taskTitle=None):
+#   if taskTitle:
+#     task = get_object_or_404(TaskModel, taskTitle=taskTitle)
+#     task.is_completed = True
+#     task.save()
+#   tasks = TaskModel.objects.filter(is_completed=True).all()
+#   return render(request, "completed_tasks.html", {"tasks": tasks})
+
+class completedTasks(ListView):
+  model = TaskModel
+  template_name = 'completed_tasks.html'
+  queryset = TaskModel.objects.filter(is_completed=True).all()
+  context_object_name = 'tasks'
+  
+  
+class MarkTaskCompleted(View):
+  def post(self, request, pk):
+    # Mark the specific task as completed
+    task = get_object_or_404(TaskModel, pk=pk)
     task.is_completed = True
     task.save()
-  tasks = TaskModel.objects.filter(is_completed=True).all()
-  return render(request, "completed_tasks.html", {"tasks": tasks})
+    # Redirect to the CompletedTasksView
+    return redirect("todolist:completed_tasks")
 
-def delete_task(request, taskTitle):
-  task = get_object_or_404(TaskModel, taskTitle=taskTitle)
-  task.delete()
-  return redirect("todolist:show_tasks")
+
+# def delete_task(request, taskTitle):
+#   task = get_object_or_404(TaskModel, taskTitle=taskTitle)
+#   task.delete()
+#   return redirect("todolist:show_tasks")
+
+class deleteTask(DeleteView):
+  model = TaskModel
+  success_url = reverse_lazy("todolist:show_tasks")
+  
+  def get(self, request, *args, **kwargs):
+    self.object = self.get_object()
+    self.object.delete()
+    return redirect(self.success_url)
